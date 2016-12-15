0be5af26782ef9a0f29cc1e22f0c6349408bdf6c
Kemal Enver <kemal.enver@sky.uk>
2016-12-15 16:37:57 +0000

    Tidying up

--- HackerRank.playground/Pages/Strings - Two Characters.xcplaygroundpage/Contents.swift
+++ HackerRank.playground/Pages/Strings - Two Characters.xcplaygroundpage/Contents.swift
@@ -1,18 +1,29 @@
 import Foundation
 
+// I will be redoing this one using a frequency list and a set of combinations
+// which I think is more efficient
+
+var inputs = ["10", "beabeefeab"]
+
+func readLine() -> String? {
+    
+    let next = inputs.first
+    inputs.removeFirst()
+    
+    return next
+}
+
 extension Character {
     
-    func unicodeScalarCodePoint() -> Int {
+    func unicodePoint() -> Int {
         
-        let characterString = String(self)
-        
-        return characterString.unicodeScalarCodePoint()
+        return String(self).unicodePoint()
     }
 }
 
 extension String {
     
-    func unicodeScalarCodePoint() -> Int {
+    func unicodePoint() -> Int {
         
         let scalars = self.unicodeScalars
         
@@ -20,38 +31,41 @@ extension String {
     }
 }
 
-let n = 10
-let s = "beabeefeab"
+let n = Int(readLine()!)
+let string = readLine()!
 
 func longestAlternatingString(s: String) -> Int {
     
-    var letters = Array(repeating: Array(repeating: Character(" "), count: 26), count: 26)
+    var characters = Array(repeating: Array(repeating: Character(" "), count: 26), count: 26)
     var values = Array(repeating: Array(repeating: 0, count: 26), count: 26)
     
     for c in s.characters {
     
         // This is basically the difference between our character and the start of the alphabet 'a'
-        // Which will give an index between 0 and 26
-        let index = c.unicodeScalarCodePoint() - 97
+        // which will give an index between 0 and 26
+        let idx = c.unicodePoint() - 97
         
         for i in 0..<26 {
             
-            if letters[index][i] != c && values[index][i] != -1 {
-                letters[index][i] = c
-                values[index][i] += 1
+            if characters[idx][i] != c && values[idx][i] != -1 {
+                
+                characters[idx][i] = c
+                values[idx][i] += 1
             } else {
-                values[index][i] = -1;
+                values[idx][i] = -1;
             }
             
-            if letters[i][index] != c && values[i][index] != -1 {
-                letters[i][index] = c
-                values[i][index] += 1
+            if characters[i][idx] != c && values[i][idx] != -1 {
+                
+                characters[i][idx] = c
+                values[i][idx] += 1
             } else {
-                values[i][index] = -1;
+                values[i][idx] = -1;
             }
         }
     }
     
+    // Find the largest string in the matrix
     var largestString = 0
     
     for i in 0..<26 {
@@ -67,4 +81,4 @@ func longestAlternatingString(s: String) -> Int {
     return largestString > 1 ? largestString : 0
 }
 
-print(longestAlternatingString(s: s))
+print(longestAlternatingString(s: string))
