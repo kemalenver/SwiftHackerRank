5e45337ac925171f1b08d429484ee93ac66bf127
Kemal Enver <kemal.enver@sky.uk>
2017-01-24 18:04:12 +0000

    WIP Analysis

--- Algorithms/Sorting.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ Algorithms/Sorting.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,7 +1,62 @@
-
+/*
+ 
+ def mergesort(ar):
+ inversions = 0
+ if len(ar) > 1:
+ mid = len(ar)/2
+ left = ar[:mid]
+ right = ar[mid:]
+ 
+ leftInv = mergesort(left)
+ rightInv =  mergesort(right)
+ inversions += leftInv
+ inversions += rightInv
+ 
+ i = 0
+ j = 0
+ k = 0
+ 
+ while i < len(left) and j < len(right):
+ if left[i] <= right[j]:
+ ar[k] = left[i]
+ i += 1
+ else:
+ ar[k] = right[j]
+ j += 1
+ print(str(inversions) + "+=" + str(mid-i) )
+ inversions += mid-i
+ k+=1
+ 
+ while i < len(left):
+ ar[k] = left[i]
+ i += 1
+ k += 1
+ 
+ while j<len(right):
+ ar[k] = right[j]
+ j += 1
+ k += 1
+ return inversions
+ 
+ raw_input = "9 1 6 3 3 8 1 2 9 1 6 3 3 8 1 2 9 1 6 3 3 8 1 2 9 1 6 3 3 8 1 2 2"
+ a = [ int( i ) for i in raw_input.strip().split() ]
+ 
+ answer = mergesort(a)
+ #print answer
+ 
+ # n = input()
+ # for iterate in range( n ):
+ #     x = input()
+ #     a = [ int( i ) for i in raw_input().strip().split() ]
+ #     answer = mergesort(a)
+ 
+ #     print answer
+ 
+ */
 import Foundation
 
-var inputs = ["2", "5", "1 1 1 2 2", "5", "2 1 3 1 2"]
+var inputs = ["1", "33", "9 1 6 3 3 8 1 2 9 1 6 3 3 8 1 2 9 1 6 3 3 8 1 2 9 1 6 3 3 8 1 2 2"]
+// 0 4 244
 
 func readLine() -> String? {
     
@@ -11,63 +66,76 @@ func readLine() -> String? {
     return next
 }
 
-var inversions = 0
+func mergeSort(_ array: [Int]) -> Int {
+    
+    var ar = array
+    
+    var inversions = 0
+    
+    if ar.count > 1 {
+        
+        let mid = ar.count / 2
 
-func merge<T: Comparable>(_ a: [T], _ b: [T], _ acc: [T]) -> [T] {
-    
-    if a.isEmpty {
+        let left = Array(array[0 ..< mid])
+        let right = Array(array[mid ..< array.count])
+
+        let leftInv = mergeSort(left)
+        let rightInv = mergeSort(right)
         
-        return acc + b
-    } else if b.isEmpty {
+        inversions += leftInv
+        inversions += rightInv
         
-        return acc + a
+        var i = 0
+        var j = 0
+        var k = 0
+        
+        while i < left.count && j < right.count {
+            
+            if left[i] <= right[j] {
+                
+                ar[k] = left[i]
+                i += 1
+            } else {
+                
+                ar[k] = right[j]
+                j += 1
+
+                print("\(inversions) += \(mid-i)")
+                inversions += mid-i
+ 
+            }
+            
+            k += 1
+        }
+        
+        while i < left.count {
+            
+            ar[k] = left[i]
+            i += 1
+            k += 1
+        }
+        
+        while j < right.count {
+            
+            ar[k] = right[j]
+            j += 1
+            k += 1
+        }
     }
     
-    let aFirst = a.first!
-    let bFirst = b.first!
-    
-    if aFirst <= bFirst {
-        
-        let allA = Array(a[1..<a.count])
-        
-        return merge(allA, b, acc + [aFirst])
-    } else {
-        
-        inversions += a.count
-        
-        let allB = Array(b[1..<b.count])
-        
-        return  merge(a, allB, acc + [bFirst])
-    }
+    return inversions
 }
 
-func mergeSort<T: Comparable>(_ a: [T]) -> [T] {
-    
-    if a.count < 2 {
-        
-        return a
-    } else {
-        
-        let mid = a.count / 2
-        
-        let firstHalf = Array(a[0 ..< mid])
-        let secondHalf = Array(a[mid ..< a.count])
-        
-        return merge(mergeSort(firstHalf), mergeSort(secondHalf), [])
-    }
-}
 
 let numberOfTests = Int(readLine()!)!
 
 for i in 0 ..< numberOfTests {
     
-    inversions = 0
-    
     let numberOfElements = Int(readLine()!)!
     
     var inputArr = readLine()!.characters.split(separator: " ").map{ Int(String($0))! }
     
-    mergeSort(inputArr)
+    let answer = mergeSort(inputArr)
     
-    print(inversions)
+    print(answer)
 }
